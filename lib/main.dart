b30562d7f229091791fd031a72c214ec3f04eeec
Muhammad Rizwan <m.rizwanmeo7862@gmail.com>
2020-11-21 04:31:28 +0500

    enabling_splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,62 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  List _fruits = ["Apple", "Banana", "Pineapple", "Mango", "Grapes"];
-
-  List<DropdownMenuItem<String>> _dropDownMenuItems;
-  String _selectedFruit;
-
-  @override
-  void initState() {
-    _dropDownMenuItems = buildAndGetDropDownMenuItems(_fruits);
-    _selectedFruit = _dropDownMenuItems[0].value;
-    super.initState();
-  }
-
-  List<DropdownMenuItem<String>> buildAndGetDropDownMenuItems(List fruits) {
-    List<DropdownMenuItem<String>> items = List();
-    for (String fruit in fruits) {
-      items.add(DropdownMenuItem(value: fruit, child: Text(fruit)));
-    }
-    return items;
-  }
-
-  void changedDropDownItem(String selectedFruit) {
-    setState(() {
-      _selectedFruit = selectedFruit;
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("DropDown Button Example"),
+          title: Text("Splash Screen Example"),
         ),
-        body: Container(
-          child: Center(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Please choose a fruit: "),
-              DropdownButton(
-                value: _selectedFruit,
-                items: _dropDownMenuItems,
-                onChanged: changedDropDownItem,
-              )
-            ],
-          )),
+        body: Center(
+          child: Text("Hello World"),
         ),
       ),
     );
